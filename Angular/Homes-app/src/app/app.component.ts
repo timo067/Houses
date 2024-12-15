db86b17439bc3ef20fee3d372f14290128375f15
timo067 <aliosman.aliosmann@gmail.com>
2024-12-16 01:19:35 +0200

    now view details work

---
+++ Angular/Homes-app/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent, RouterModule
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
